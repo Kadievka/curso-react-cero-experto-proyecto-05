69c54399001d7a9e072ba12172edffc236a78679
Kadievka Salcedo <kadieka.s.ks@gmail.com>
2022-05-10 19:24:29 +0200

    Memo - Método de React

---
+++ src/components/06-memos/Small.tsx
@@ -0,0 +1,16 @@
+import React, { memo } from 'react'
+
+export interface SmallInputInterface {
+  value: number
+}
+
+const Small = memo(({ value }: SmallInputInterface) => {
+
+  console.log("Me volví a llamar :'(");
+
+  return (
+    <small> { value } </small>
+  )
+});
+
+export default Small
--- src/index.tsx
+++ src/index.tsx
@@ -9,12 +9,13 @@ import FormWithCustomHook from './components/02-useEffect/FormWithCustomHook';
 import MultipleCustomHooks from './components/03-examples/MultipleCustomHooks';
 import FocusScreen from './components/04-useRef/FocusScreen';
 import RealExampleRef from './components/04-useRef/RealExampleRef';
-*/
 import Layout from './components/05-useLayoutEffect/Layout';
+*/
+import Memorize from './components/06-memos/Memorize';
 import './index.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render(<Layout />);
+root.render(<Memorize />);
 

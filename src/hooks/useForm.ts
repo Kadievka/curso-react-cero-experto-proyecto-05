072b16e9d698b01fa5d3bd8af338de3416d2e222
Kadievka Salcedo <kadieka.s.ks@gmail.com>
2022-05-10 11:45:43 +0200

    Formulario con custom Hook

---
+++ src/hooks/useForm.ts
@@ -0,0 +1,19 @@
+import { useState } from "react";
+
+export const useForm = (initialState: any = {} ) => {
+
+  const [values, setValues] = useState(initialState);
+
+  const handleInputChange = ({ target }: React.ChangeEvent<HTMLInputElement>) => {
+
+    console.log(target.name, target.value);
+
+    setValues(() => ({
+      ...values,
+      [target.name]: target.value
+    }));
+
+  }
+
+  return [ values, handleInputChange];
+}

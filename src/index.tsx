b5dbde423adc0325f0f826bf8d5552490427667c
Kadievka Salcedo <kadieka.s.ks@gmail.com>
2022-05-10 20:37:10 +0200

    Tarea Memorize

--- src/index.tsx
+++ src/index.tsx
@@ -14,11 +14,13 @@ import RealExampleRef from './components/04-useRef/RealExampleRef';
 import Layout from './components/05-useLayoutEffect/Layout';
 import Memorize from './components/06-memos/Memorize';
 import MemoHook from './components/06-memos/MemoHook';
-*/
 import CallbackHook from './components/06-memos/CallbackHook';
+
+*/
+import { Padre } from './components/07-tarea-memo/Padre';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render(<CallbackHook />);
+root.render(<Padre />);
 

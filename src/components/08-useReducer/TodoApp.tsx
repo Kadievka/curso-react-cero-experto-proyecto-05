dae991de008cc694c892b2c0da858502808ad596
Kadievka Salcedo <kadieka.s.ks@gmail.com>
2022-05-11 15:06:59 +0200

    Creando el cascarón de la lista de TODOs

--- src/components/08-useReducer/TodoApp.tsx
+++ src/components/08-useReducer/TodoApp.tsx
@@ -14,18 +14,62 @@ const TodoApp = () => {
 
   const [todos] = useReducer(todoReducer, initialState);
 
-  console.log(todos)
+  console.log(todos);
 
   return (
     <div>
-      <h1>TodoApp</h1>
+      <h1>TodoApp ( { todos?.length } ) </h1>
       <hr />
 
-      <ul>
-        <li>Hola</li>
-        <li>Mundo</li>
-        <li>Hola de nuevo</li>
-      </ul>
+      <div className="row">
+
+        <div className="col-7">
+          <ul className="list-group-flush">
+          {
+            todos?.map( ({id, description}, index) => {
+              return (
+                <li
+                  key={id}
+                  className="list-group-item"
+                >
+                  <p className="text-center">
+                    { index + 1 }. { description }
+                  </p>
+                  <button
+                    className="btn btn-danger"
+                  >
+                    Borrar
+                  </button>
+                </li>
+              )
+            })
+          }
+        </ul>
+        </div>
+
+        <div className="col-5">
+
+          <h4>Agregar TODO</h4>
+          <hr />
+
+          <form>
+            <input
+              autoComplete="off"
+              className="form-control"
+              name="description"
+              placeholder="Aprender ..."
+              type="text"
+            />
+            <button
+              className="btn btn-outline-primary mt-1 w-100"
+            >
+              Agregar
+            </button>
+          </form>
+
+        </div>
+
+      </div>
 
     </div>
   )
